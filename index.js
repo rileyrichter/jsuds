312bee9fe82bf5ecf1d38eac88d51f003e153add
rileyrichter <ben@webflow.com>
2023-07-19 20:57:59 -0500

    Fixing mobile swiper issues

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const seasonSelect = document.querySelector("#season");
 const motionState = window.matchMedia("(prefers-reduced-motion: reduce)");
-const swiperWrapper = document.querySelector(".swiper-wrapper");
-
-// Mapping seasons to steps from the first slide
-const slideSteps = {
-  1: 24,
-  2: 12,
-  3: 0,
-  default: 0,
-};
 
 window.addEventListener("load", () => {
-  swiperWrapper.classList.remove("cls");
-
+  document.querySelector(".swiper-wrapper").classList.remove("cls");
   const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: false,
     slidesPerView: 3,
     spaceBetween: 10,
     slidesPerGroup: 3,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+
     breakpoints: {
       992: {
         slidesPerView: 3,
@@ -45,71 +38,59 @@ window.addEventListener("load", () => {
     },
   });
 
-  seasonSelect.onchange = function () {
-    const season = Number(this.value);
-    const slideStep = slideSteps[season] || slideSteps.default;
-    const duration = motionState.matches ? 0 : 500;
-
-    // Sliding to the step away from the first slide
-    swiper.slideTo(swiper.activeIndex + slideStep, duration, true);
+  const slideIndices = {
+    1: 25,
+    2: 13,
+    3: 1,
+    default: 1,
   };
-});
 
-/*
+  const handleSeasonChange = function () {
+    const season = Number(seasonSelect.value);
+    let slideIndex = slideIndices[season] || slideIndices.default;
 
-const seasonSelect = document.querySelector("#season");
-const motionState = window.matchMedia("(prefers-reduced-motion: reduce)");
-const swiperWrapper = document.querySelector(".swiper-wrapper");
-
-const slideIndices = {
-  1: 25,
-  2: 13,
-  3: 1,
-  default: 1,
-};
-
-window.addEventListener("load", () => {
-  swiperWrapper.classList.remove("cls");
-
-  const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: false,
-    slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 3,
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 16,
-        slidesPerGroup: 3,
-      },
-      479: {
-        slidesPerView: 2,
-        spaceBetween: 16,
-        slidesPerGroup: 2,
-      },
-      0: {
-        slidesPerView: 1.2,
-        spaceBetween: 16,
-        slidesPerGroup: 1,
-      },
-    },
-    pagination: {
-      el: ".pagination",
-    },
-    navigation: {
-      nextEl: ".next",
-      prevEl: ".prev",
-    },
-  });
-
-  seasonSelect.onchange = function () {
-    const season = Number(this.value);
-    const slideIndex = slideIndices[season] || slideIndices.default;
     const duration = motionState.matches ? 0 : 500;
+
+    // Adjust slideIndex for viewport widths of 478px or less
+    if (window.innerWidth <= 478) {
+      slideIndex -= 1;
+    }
 
     swiper.slideTo(slideIndex, duration, true);
   };
-});
 
-*/
+  // Call on window load
+  window.onload = handleSeasonChange;
+
+  // Call on window resize
+  window.onresize = handleSeasonChange;
+
+  // Bind to seasonSelect change event
+  seasonSelect.onchange = handleSeasonChange;
+
+  // seasonSelect.onchange = function () {
+  //   const season = Number(this.value);
+
+  //   if (motionState.matches) {
+  //     if (season === 1) {
+  //       swiper.slideTo(25, 0, true);
+  //     } else if (season === 2) {
+  //       swiper.slideTo(13, 0, true);
+  //     } else if (season === 3) {
+  //       swiper.slideTo(1, 0, true);
+  //     } else {
+  //       swiper.slideTo(1, 0, true);
+  //     }
+  //   } else {
+  //     if (season === 1) {
+  //       swiper.slideTo(25, 500, true);
+  //     } else if (season === 2) {
+  //       swiper.slideTo(13, 500, true);
+  //     } else if (season === 3) {
+  //       swiper.slideTo(1, 500, true);
+  //     } else {
+  //       swiper.slideTo(1, 500, true);
+  //     }
+  //   }
+  // };
+});
